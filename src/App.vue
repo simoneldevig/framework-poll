6acc7b0ae6601e7c69bf1c154e97d881911a2afd
Simon Eldevig <Simon.eldevig@lego.com>
2024-01-26 14:45:38 +0100

    Remove unused classes

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@
       <h2 class="text-center text-lg font-semibold">Total {{ votes.vue + votes.react }} votes</h2>
       <button
         @click="vote('none')"
-        class="self-center focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+        class="self-center focus:outline-none text-white bg-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 mb-2"
       >
         Remove your vote {{ voted !== 'none' ? `(${ voted })` : '' }}
       </button>
